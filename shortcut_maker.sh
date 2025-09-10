2885c7b60eb4c00dca2e02cb18cc6bf4e584fd2d
FYKyle <53303198+FYKyle@users.noreply.github.com>
2025-09-09 21:06:23 -0500

    Add files via upload

--- shortcut_maker.sh
+++ shortcut_maker.sh
@@ -4,6 +4,9 @@
 ######################
 # creates shortcuts :P
 # that aren't aliases
+#--- set up ---
+gameshortcut_dir="$HOME/games-on-yo-phone/__gameshortcuts__/"
+printf -v currentdir "%q" "$PWD"
 #--- Preperations --- 
 echo -ne 'Game: '; read game
 game=$game
@@ -12,9 +15,9 @@ game_lower=${gamenospace,,}
 echo -ne 'Platform: '; read platform
 #--- LINUX ---
 if [ $platform == 'linux' ]; then 
-echo -ne "Path: "; read path
-echo -ne "Executable: "; read executable
-command="cd $path && nohup ./$executable &> /dev/null"
+echo -ne "Executable ?> "; read executable
+printf -v executable "%q" "$executable"
+command="cd $currentdir && nohup ./$executable &> /dev/null"
 fi
 #--- MAME --- 
 if [ $platform == 'mame' ]; then 
@@ -27,10 +30,19 @@ if [ $platform == 'steam' ]; then
 echo -ne "Steam ID: "; read steamid
 command="nohup steam -silent steam://rungameid/${steamid} &> /dev/null & disown"
 fi
+#--- WINE --- 
+if [ $platform == 'wine' ]; then 
+ls ~/.local/share/wineprefixes
+echo -ne "Prefix: "; read prefix
+echo -ne "Executable ?> "; read executable
+printf -v executable "%q" "$executable"
+command="WINEPREFIX=~/.local/share/wineprefixes/$prefix wine $currentdir/$executable &> /dev/null & disown"
+fi
 #--- WRITING FILE ---
-cat > $game_lower << EOF
+shortcut=$gameshortcut_dir$game_lower
+cat > $shortcut << EOF
 echo -e "[\e[32mPLAYING\e[0m] ${game^^}"
 $command
 EOF
 #---
-chmod +x $game_lower
+chmod +x $shortcut
